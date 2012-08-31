9081735c5595348502681b946f32a6275b782a2a
moolsan <github@lotus-continuum.de>
2012-08-31 09:37:31 +0200

    merged

--- thot-app/src/main/java/org/ale/thot/domain/StaticTimeslotDao.java
+++ thot-app/src/main/java/org/ale/thot/domain/StaticTimeslotDao.java
@@ -1,30 +1,31 @@
 package org.ale.thot.domain;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StaticTimeslotDao implements TimeslotDao {
 	
 	private Map<String, List<String>> timeslotsPerDay;
 	
 	public StaticTimeslotDao() {
-		timeslotsPerDay = new HashMap<String, List<String>>();
+		timeslotsPerDay = new HashMap<String, List<Timeslot>>();
 		
-		List<String> wedTimeSlots = new ArrayList<String>();
-		wedTimeSlots.add("15:30 - 16:15");
-		wedTimeSlots.add("16:15 - 17:00");
-		wedTimeSlots.add("Evening");		
+		List<Timeslot> wedTimeSlots = new ArrayList<Timeslot>();
+		wedTimeSlots.add(new Timeslot("15:30", "16:15"));
+		wedTimeSlots.add(new Timeslot("16:15", "17:00"));
+		wedTimeSlots.add(new Timeslot("Evening"));		
 
-		List<String> thuTimeSlots = new ArrayList<String>();
-		thuTimeSlots.add("15:30 - 16:15");
-		thuTimeSlots.add("16:15 - 17:00");
-		thuTimeSlots.add("17:00 - 17:45");
-		thuTimeSlots.add("Evening");
+		List<Timeslot> thuTimeSlots = new ArrayList<Timeslot>();
+		thuTimeSlots.add(new Timeslot("15:30", "16:15"));
+		thuTimeSlots.add(new Timeslot("16:15", "17:00"));
+		thuTimeSlots.add(new Timeslot("17:00", "17:45"));
+		thuTimeSlots.add(new Timeslot("Evening"));
 		
-		List<String> friTimeSlots = new ArrayList<String>();
-		friTimeSlots.add("15:30 - 16:15");
-		friTimeSlots.add("16:15 - 17:00");
-		friTimeSlots.add("17:15 - 18:00");
-		friTimeSlots.add("Evening");
+		List<Timeslot> friTimeSlots = new ArrayList<String>();
+		friTimeSlots.add(new Timeslot("11:30 - 12:10"));
+		friTimeSlots.add(new Timeslot("12:15 - 13:00"));
 		
 		timeslotsPerDay.put("Wed", wedTimeSlots);
 		timeslotsPerDay.put("Thu", thuTimeSlots);
