9109f31e8651299db78a7c326558730d3ccc9220
Henning Wielenberg <hewie@tzi.de>
2012-11-29 17:07:49 +0100

    added jBehave tutorial Test

---
+++ thot-app/src/test/java/org/ale/thot/selenium/StringRenderer.java
@@ -0,0 +1,7 @@
+package org.ale.thot.selenium;
+
+public class StringRenderer {
+	public String asString() {
+		return null;  //To change body of created methods use File | Settings | File Templates.
+	}
+}
