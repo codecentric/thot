ccf582e160d91155ebb566272cdd70a0ae0ccfad
Marcus Krassmann <marcus.krassmann@gmail.com>
2012-11-29 18:13:48 +0100

    Removed generated class files

--- thot-app/bin/src/main/resources/sql/init-db.sql
+++
@@ -1,9 +0,0 @@
-create database thot;
-
-create user thot@'%' identified by 'thot';
-create user thot@'localhost' identified by 'thot';
-
-grant all on thot.* to thot@'%';
-grant all on thot.* to thot@'localhost';
-
-flush privileges;
--- thot-app/bin/src/main/webapp/assets/js/thot.js
+++
@@ -1,8 +0,0 @@
-// Used by static sessions
-(function () {
-    // Enables detail links for the session overview
-    $("table.table tbody tr.sessions").on("click", function () {
-        var url = $(this).attr("data-link");
-        window.location = url;
-    });
-}());
